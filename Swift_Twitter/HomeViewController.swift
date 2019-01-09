db0a4b7bbe69420ba930cbe8c6482ef5f1985666
nghianguyenHD <nghia.nguyen2@hdwebsoft.co>
2019-01-09 17:59:41 +0700

    [COMMIT] - install LBTAComponents pod

---
+++ Swift_Twitter/HomeViewController.swift
@@ -0,0 +1,30 @@
+//
+//  HomeViewController.swift
+//  Swift_Twitter
+//
+//  Created by HD WebSoft on 1/9/19.
+//  Copyright © 2019 HD WebSoft. All rights reserved.
+//
+
+import UIKit
+
+class HomeViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
